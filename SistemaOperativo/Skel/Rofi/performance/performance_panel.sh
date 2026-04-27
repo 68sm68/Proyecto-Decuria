d77cb0e6bf4f47e7c09fed6a509fab8f6ee1526d
sleon021 <sergi.leon@elpuig.xeill.net>
2026-04-27 19:27:00 +0200

    performance_panel.sh

--- SistemaOperativo/Skel/Rofi/performance/performance_panel.sh
+++ SistemaOperativo/Skel/Rofi/performance/performance_panel.sh
@@ -3,9 +3,16 @@
 SCRIPT_DIR="$HOME/.config/performance/scripts"
 ROFI_THEME="$HOME/.config/rofi/systemctl_simple.rasi"
 
-OPTIONS="📊 Rendimiento y Hardware\n🧹 Gestion del Sistema\n⚙️ Cambiar Governor (CPU)\n🔀 Ajustar Swappiness (RAM)\n🛑 Controlar Servicios (RAM)\n🚀 Ajuste de Latencia (Red)\n🖥️ Alternar Compositor (Picom)"
-
-CHOICE=$(echo -e "$OPTIONS" | rofi -dmenu \
+CHOICE=$(printf "%s\n" \
+    "📊 Rendimiento y Hardware" \
+    "⌘  Gestion del Sistema" \
+    "💾 Gestionar ZRAM" \
+    "⚙️ Cambiar Governor (CPU)" \
+    "🔀 Ajustar Swappiness (RAM)" \
+    "🛑 Controlar Servicios (RAM)" \
+    "🚀 Ajuste de Latencia (Red)" \
+    "🖥️ Alternar Compositor (Picom)" | \
+    rofi -dmenu \
     -theme "$ROFI_THEME" \
     -p "PANEL DE RENDIMIENTO AVANZADO")
 
@@ -13,8 +20,11 @@ case "$CHOICE" in
     "📊 Rendimiento y Hardware")
         "$SCRIPT_DIR/hardware_info.sh"
         ;;
-    "🧹 Gestion del Sistema")
+    "⌘  Gestion del Sistema")
         "$SCRIPT_DIR/system_manager.sh"
+        ;;
+    "💾 Gestionar ZRAM")
+        "$SCRIPT_DIR/zram_manager.sh"
         ;;
     "⚙️ Cambiar Governor (CPU)")
         "$SCRIPT_DIR/set_governor.sh"
